608848bf48b1cb2b7127e886022aa4e7afa467fb
Laurelie <laurelie.michielon@gmail.com>
2018-04-23 19:37:12 +0200

    Update Animaux.java

--- Animaux.java
+++ Animaux.java
@@ -1,2 +1,13 @@
 public class abstract Animaux implements mangeable{
+  protected int poids;
+  public Animaux(int poids){
+    this.poids = poids;
+  }
+  public abstract int estMange();
+  public int getPoids(){
+    return poids;
+  }
+  public String toString(){
+    return ;
+  }
 }
