22975b38705cee60f62d94b8f5f194735f814088
Laurelie <laurelie.michielon@gmail.com>
2018-04-23 19:18:30 +0200

    Update Personnage.java

--- Personnage.java
+++ Personnage.java
@@ -4,11 +4,14 @@ public class Personnage{
   private ArrayList<Lieu> carte = new ArrayLsit<Lieu>();
   private String nom;
   private ArrayList<? extends Stockage> inventaire = new ArrayList<? extends Stockage>();
+  private int poidsIventaire; 
+  private final int poidsMaxInventaire;
   private int energie;
   private int sante;
   private int x;
   private int y;
   public Personnage(String nom){
+    poids = 0;
     this.nom = nom;
     energie = 100;
     sante = 100;
@@ -23,6 +26,14 @@ public class Personnage{
   }
   public int getSante(){
     return sante;
+  }
+  public void ramasser(? extends Stockable objet){
+    if (poidsInventaire+objet.getPoids() >= poidsMaxInventaire)
+      System.out.println("Vous ne pouvez pas stocker cet objet ("+objet.getPoids()+") : "+poidsInventaire+"/"+poidsMaxInventaire)
+    else{
+      inventaire.add(objet);
+      poidsInventaire += objet.getPoids();
+    }
   }
   public ArrayList<? extends Stockage> getInventaire(){
     return inventaire;      
@@ -41,8 +52,5 @@ public class Personnage{
   }
   public void pecher(){
   
-  }
-  public void ramasser(? extends Stockable objet){
-    inventaire.add(objet);
   }
 }
