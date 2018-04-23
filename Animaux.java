2aaf7542e4e522b17c80dd7b56098593e6474392
Laurelie <laurelie.michielon@gmail.com>
2018-04-23 20:13:30 +0200

    Update Animaux.java

--- Animaux.java
+++ Animaux.java
@@ -1,13 +1,17 @@
 public class abstract Animaux implements mangeable{
   protected int poids;
-  public Animaux(int poids){
+  protected String nom;
+  private int rapportAlimentaire;
+  public Animaux(String nom, int poids, int rapportAlimentaire){
     this.poids = poids;
+    this.nom = nom;
+    this.rapportAlimentaire = rapportAlimentaire;
   }
-  public abstract int estMange();
   public int getPoids(){
     return poids;
   }
   public String toString(){
-    return ;
+    return "C'est un "+nom+" de poids :"+poids+" qui équivault à "+rapportAlimentaire+" de rapportAlimentaire";
   }
+  public abstract int estMange();
 }
