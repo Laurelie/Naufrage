7f7ec86fdf536cadb1c0847fa6dfd275529d30bd
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 10:07:16 +0200

    Update Chassable.java

--- Chassable.java
+++ Chassable.java
@@ -1,5 +1,5 @@
 public interface Chassable{
   public void chasser( Personnage p);
   public void partir(Personnage p);
-  public void apprivoiser(Personnage p, Mangeable aliment);
+  public void apprivoiser(Personnage p, ? implements Mangeable aliment);
 }
