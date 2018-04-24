1e2286ce4776c0ae13e754b584691651700b7d31
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 10:16:14 +0200

    Update Animaux.java

--- Animaux.java
+++ Animaux.java
@@ -1,11 +1,15 @@
 public class abstract Animaux implements mangeable{
-  protected int poids;
+  private int santé;
+  private int poids;/* vu qu'on a un accesseur on peut le mettre private*/
   protected String nom;
   private int rapportAlimentaire;
   public Animaux(String nom, int poids, int rapportAlimentaire){
     this.poids = poids;
     this.nom = nom;
     this.rapportAlimentaire = rapportAlimentaire;/* en vrai on a pas besoin de rapport alimentaire si on a la fonction estMange() pour chaque animal*/
+  }
+  public int getSanté(){
+    return santé;
   }
   public int getPoids(){
     return poids;
