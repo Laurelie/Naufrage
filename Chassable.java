fcc70529727deb57230673b6fd11a6a259af2c0e
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 13:38:29 +0200

    Update Chassable.java

--- Chassable.java
+++ Chassable.java
@@ -1,6 +1,6 @@
 public interface Chassable{
   public void reagirAttaque( Personnage p);
-  public void reagirfuite(Personnage p);
+  public void reagirFuite(Personnage p);
   public void descriptionMort();
   /*public void reagirapprivoiser(Personnage p, ? implements Mangeable aliment);*/
 }
