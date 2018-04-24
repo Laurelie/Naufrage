550314f6d1d09a62bb524caf45482ee78d251768
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 10:33:37 +0200

    Create Lance.java

---
+++ Lance.java
@@ -0,0 +1,13 @@
+public class Lance extends Arme{
+   public Lance{
+    super(3,0.7);
+    }
+    
+   public void descriptionEchec{
+    System.out.println("Votre lance passe à coté de l'adversaire.");
+    }
+    
+    public void descriptionReussite{
+      System.out.println("Vous touchez l'adversaire et vous lui infligez 3 blessures");
+      }
+  }
