4bf465eec3bd65de676b6351b4682145a10cd923
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 10:35:23 +0200

    Update Arme.java

--- Arme.java
+++ Arme.java
@@ -1,6 +1,10 @@
 public abstract class Arme{
   private int degat;
   private float precision;
+  protected Arme( int degat, float precision){
+    this.degat=degat;
+    this.precision=precision;
+  }
   public void descriptionEchec();
   public void descriptionReussite();
  }
