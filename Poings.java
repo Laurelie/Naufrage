0efe7f1edb6687640c7a459592402abfe56d15e2
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 13:43:36 +0200

    Create Poings.java

---
+++ Poings.java
@@ -0,0 +1,11 @@
+public class Poings extends Arme{
+  public Poings(){
+    super(1,0.5,"Poings");
+    }
+  public void descriptionEchec(){
+    System.out.println("Votre coup de poing passe à coté de l'animal.");
+    }
+  public void descriptionReussite(){
+    System.out.println("Vous assènez un sévère coup de poing à l'animal");
+    }
+   }
