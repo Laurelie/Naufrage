5322993ecc23e9142616b7f4fe8ff198fefffbd8
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-24 18:19:20 +0200

    Update Foret.java

--- Foret.java
+++ Foret.java
@@ -1,3 +1,13 @@
 public class Foret extends Lieu{
-
+  public Foret(){
+    super("Foret");
+  }
+  public void choixAction(){};
+  public void genererAnimal(){
+    if(Math.random<0.3){
+      this.changerAnimal(new Ours());
+    }
+  }
+  public void decrireLieu(){
+    System.out.println("Vous avancer dans une foret dense.");
 }
