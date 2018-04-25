4c8110f41c7ea8a21d15b8c7883759c824c20749
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-25 16:16:17 +0200

    Update Foret.java

--- Foret.java
+++ Foret.java
@@ -4,10 +4,12 @@ public class Foret extends Lieu{
   }
   public void choixAction(){};
   public void genererAnimal(){
-    if(Math.random<0.3){
+    if(Math.random()<0.3){
       this.changerAnimal(new Ours());
+      System.out.println("Vous apercevez un ours derrière un buisson.");
     }
   }
   public void decrireLieu(){
     System.out.println("Vous avancer dans une foret dense.");
+}
 }
