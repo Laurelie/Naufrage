775490eba1f0258900c4d995d4761ac83aa65e1a
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-25 16:16:56 +0200

    Update Lieu.java

--- Lieu.java
+++ Lieu.java
@@ -1,17 +1,17 @@
-public class abstract Lieu{
-  private Animal occupant;
+public abstract class Lieu{
+  private AnimauxChassable occupant;
   private String nom;
-  public Lieu(String nom){
+  protected Lieu(String nom){
     this.nom = nom;
     occupant = null;
   }
   public abstract void choixAction();
   public abstract void genererAnimal();
   public abstract void decrireLieu();
-  public void changerAnimal(Animal nouvelOccupant){
+  public void changerAnimal(AnimauxChassable nouvelOccupant){
     occupant = nouvelOccupant;
   }
-  public Animaux getOccupant(){
+  public AnimauxChassable getOccupant(){
     return(occupant);
   }
   public String getNom(){
