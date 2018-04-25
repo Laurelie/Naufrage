a257391ecad272fece043482a842808a4df0c98a
Laurelie <laurelie.michielon@gmail.com>
2018-04-25 22:21:08 +0200

    Update Animaux.java

--- Animaux.java
+++ Animaux.java
@@ -1,18 +1,55 @@
-public abstract class Animaux /*implements mangeable*/{
-  private int poids;/* vu qu'on a un accesseur on peut le mettre private*/
-  protected String nom;
-  private int rapportAlimentaire;
-  public Animaux(String nom, int poids, int rapportAlimentaire){
-    this.poids = poids;
-    this.nom = nom;
-    this.rapportAlimentaire = rapportAlimentaire;/* en vrai on a pas besoin de rapport alimentaire si on a la fonction estMange() pour chaque animal*/
-  }
-  
-  public int getPoids(){
-    return poids;
-  }
-  public String toString(){
-    return "C'est un "+nom+" de poids :"+poids+" qui équivault à "+rapportAlimentaire+" de rapportAlimentaire.";
-  }
-  /*public abstract int estMange();*/
+public abstract class Animaux implements Mangeable{
+	private int poids;/* vu qu'on a un accesseur on peut le mettre private*/
+	protected String nom;
+	private int rapportAlimentaire;
+	private boolean vivant;
+	private boolean cru;
+	public Animaux(String nom, int poids, int rapportAlimentaire, boolean vivant, boolean cru){
+	  	this.poids = poids;
+	  	this.nom = nom;
+	  	this.rapportAlimentaire = rapportAlimentaire;
+		this.vivant = vivant;
+		this.cru = cru;
+	}
+	public void setRapportAlimentaire(int rapportAlimentaire){ //LAU
+		rapportAlimentaire = this.rapportAlimentaire;
+	}
+	public void setVivant(boolean vivant){ //LAU
+		this.vivant = vivant;
+	}
+	public boolean getVivant(){ //LAU
+		return vivant;
+	}
+	public void setCru(boolean cru){ //LAU
+		this.cru = cru;
+	}
+	public int getPoids(){ //LAU
+	  	return poids;
+	}
+	public String toString(){ //LAU
+	  	String txt = nom+" de poids :"+poids+" qui équivault à "+rapportAlimentaire+" de rapportAlimentaire. Cet animal est ";
+	  	if(vivant)
+	    	return txt+"vivant";
+		txt+="mort et ";
+		if(cru)
+	    	return txt+"cru";
+	  	return txt+"cuisiné";
+	}
+	public int estMange(Personnage p){ //LAU
+		if (cru){
+	    	System.out.println("Vous mangez un "+nom+" cru : -"+rapportAlimentaire+" de santé");
+	    	return 0-rapportAlimentaire;
+	  	}
+	  	else{
+	    	System.out.println("Vous mangez un "+nom+" cuisiné : +"+rapportAlimentaire+" de santé");
+	    	return rapportAlimentaire;
+	  	}
+	}
+	public void estCuisine(){ //LAU
+	  	if (!vivant){ //vivant == False
+	    	cru = false;
+	  	}
+	  	else
+	    	System.out.println("On ne peut pas cuisiné un "+nom+" vivant");
+	}
 }
