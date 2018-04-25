5ea76bcd7abf869e93868c6ee19ca83277193635
Laurelie <laurelie.michielon@gmail.com>
2018-04-25 22:35:38 +0200

    Update AnimauxChassable.java

--- AnimauxChassable.java
+++ AnimauxChassable.java
@@ -1,7 +1,7 @@
-public abstract class AnimauxChassable extends Animaux implements Chassable {
+public abstract class AnimauxChassable extends Animaux {
 	 private int sante;
 	 protected AnimauxChassable(String nom, int poids, int rapportAlimentaire, int sante){
-		 super(nom,poids,rapportAlimentaire);
+		 super(nom,poids,rapportAlimentaire,true,true); //ajout pour vivant et cru
 		 this.sante = sante;
 	 }
 	 public int getSante(){
@@ -9,5 +9,11 @@ public abstract class AnimauxChassable extends Animaux implements Chassable {
 		  }
 	 public void perdSante(int nb){
 		    sante=sante-nb;
+		    if (sante<=0)
+		    	this.setVivant(false);
 		  }
+  	public abstract void reagirAttaque( Personnage p);
+  	public abstract void reagirFuite(Personnage p);
+  	public abstract void descriptionMort();
+  	/*public void reagirapprivoiser(Personnage p, ? implements Mangeable aliment);*/
 }
