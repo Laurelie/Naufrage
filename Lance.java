bd4ee48330a4afc7ef10a784694fc26adc8f8afd
Laurelie <laurelie.michielon@gmail.com>
2018-04-25 22:39:53 +0200

    Update Lance.java

--- Lance.java
+++ Lance.java
@@ -1,13 +1,13 @@
 public class Lance extends Arme{
-   public Lance{
-    super(3,0.7);
-    }
+   public Lance(){
+      super(3,0.7,"Lance");
+   }
     
-   public void descriptionEchec{
-    System.out.println("Votre lance passe à coté de l'adversaire.");
-    }
+   public void descriptionEchec(){
+      System.out.println("Votre lance passe à coté de l'adversaire.");
+   }
     
-    public void descriptionReussite{
+   public void descriptionReussite(){
       System.out.println("Vous touchez l'adversaire et vous lui infligez 3 blessures");
-      }
-  }
+   }
+}
