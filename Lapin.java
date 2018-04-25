6fef18019abeed88496c1888563afa5686a763a8
Laurelie <laurelie.michielon@gmail.com>
2018-04-25 22:37:12 +0200

    Update Lapin.java

--- Lapin.java
+++ Lapin.java
@@ -1,35 +1,21 @@
-public class Lapin extends Animaux implements Mangeable, Chassable{
-  private boolean vivant;
-  private boolean cru;
-  public Lapin(){
-    int poids = (int)(Math.random()*20+10); //poidsMax = 30
-    int rapportAlimentaire = poids*20/30; //20 de rapportAlim si poids = 30
-    super("Lapin",poids,rapportAlimentaire);
-    vivant = True;
-    cru = True;
-  }
-  public void estCuisine(){
-    if (!vivant){ //vivant == False
-      cru = False
-    }
-    else
-      System.out.println("On ne peut pas cuisiné un Lapin vivant");
-  }
-  public int estMange(){
-    if (cru){
-      System.out.println("Vous mangez un lapin cru : -"+rapportAlimentaire+" de santé");
-      return rapportAlimentaire;
-    }
-    else{
-      System.out.println("Vous mangez un lapin cuisiné : +"+rapportAlimentaire+" de santé");
-    }
-  }
-  public String toString(){
-    String txt =super.toString();
-    if(vivant)
-      return txt+"Il est vivant."
-    if(cru)
-      return txt+"Il est mort et cru."
-    return txt+"Il est mort et cuisiné";
-  }
+public class Lapin extends AnimauxChassable{
+  	public Lapin(){
+    	super("Lapin",(int)(Math.random()*20+10),20,(int)(Math.random()*10)+10);
+  	}
+  	public void reagirAttaque( Personnage p){
+    	if(Math.random()<0.7){
+      		System.out.println("Le lapin saute dans tous les sens sans vous effleurer");
+    	}
+    	else{
+    		System.out.println("Vous vous faîtes mordre le pieds. Vous retire 3 PV");
+      		p.modifierSante(-3);
+    	}
+  	}
+  	public void reagirFuite(Personnage p) {
+    	System.out.println("Le lapin lapine");
+  	}
+  	public void descriptionMort(){
+     	System.out.println("Le lapin suffoque et meurt");
+     	setVivant(false);
+  	}
 }
