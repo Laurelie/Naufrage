5dda00d6fe104b73ccf8a81e3bcb49cc5bdee5bd
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-25 16:10:54 +0200

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,15 +1,19 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class Action{
-   Scanner sc = new Scanner(System.in);
+   static Scanner sc = new Scanner(System.in);
    public static void agir(Personnage p){
-        boolean nord,sud,est,ouest,commandeExecutee = false;
+        boolean nord = false;
+        boolean sud = false;
+        boolean est = false;
+        boolean ouest = false;
+        boolean commandeExecutee = false;
         Lieu[][] carte = p.getCarte();
         int x =p.getX();
         int y =p.getY();
         Lieu position = carte[x][y];
         if(position.getOccupant()!=null){
             System.out.println("1) Fuir \n2) Chasser \n3)Ignorer");
-            int str = sc.nextLine();
+            int str = Integer.parseInt(sc.nextLine());
             if(str==1){
                p.fuir();
              }
@@ -17,67 +21,67 @@ public class Action{
                p.combattre();
               }
             if(str==3){
-               p.ignorer()
+               p.ignorer();
                }
             if(str!=1 && str!=2 && str!=3){
                System.out.println("Commande introuvable.");
               }
-            Action.agir();
+            Action.agir(p);
             }
          else{
-            if(not(carte[x][y-1].getNom()).equals("Mer")){
+            if(!((carte[x][y-1].getNom()).equals("Mer"))){
                System.out.println("1) Aller au Nord.");
                nord = true;
                }
              else{
-               System.out.println("1) Vous êtes bloqué par la mer au Nord"
+               System.out.println("1) Vous êtes bloqué par la mer au Nord");
                }
-             if(not(carte[x][y+1].getNom()).equals("Mer")){
+             if(!((carte[x][y+1].getNom()).equals("Mer"))){
                System.out.println("2) Aller au Sud.");
                sud = true;
                }
              else{
-               System.out.println("2) Vous êtes bloqué par la mer au Sud"
+               System.out.println("2) Vous êtes bloqué par la mer au Sud");
                }
-             if(not(carte[x-1][y].getNom()).equals("Mer")){
+             if(!(carte[x-1][y].getNom()).equals("Mer")){
                System.out.println("3) Aller a l'ouest.");
                ouest = true;
                }
              else{
-               System.out.println("3) Vous êtes bloqué par la mer a l'ouest."
+               System.out.println("3) Vous êtes bloqué par la mer a l'ouest.");
                }
-             if(not(carte[x+1][y].getNom()).equals("Mer")){
+             if(!(carte[x+1][y].getNom()).equals("Mer")){
                System.out.println("4) Aller a l'est.");
                est = true;
                }
              else{
-               System.out.println("4) Vous êtes bloqué par la mer a l'est."
+               System.out.println("4) Vous êtes bloqué par la mer a l'est.");
                }
-              System.out.println("5) Manger. \n6) Fabriquer \n7) Afficher la carte \n8) Afficher son statut \n9) Consulter l'inventaire\n10) Actions spécifiques au lieu")
-              int str = sc.nextLine();
+              System.out.println("5) Manger. \n6) Fabriquer \n7) Afficher la carte \n8) Afficher son statut \n9) Consulter l'inventaire\n10) Actions spécifiques au lieu");
+              int str = Integer.parseInt(sc.nextLine());
               if(str==1 && nord){
                p.seDeplacer("Nord");
-               commandeExecutee=true,
+               commandeExecutee=true;
                }
               if(str==2 && sud){
                p.seDeplacer("Sud");
-               commandeExecutee=true,
+               commandeExecutee=true;
                }
               if(str==3 && est){
                p.seDeplacer("Est");
-               commandeExecutee=true,
+               commandeExecutee=true;
                }
               if(str==4 && ouest){
                p.seDeplacer("Ouest");
-               commandeExecutee=true,
+               commandeExecutee=true;
                }
               if(str==5){
                p.manger();
-               commandeExecutee=true,
+               commandeExecutee=true;
                }
-              if(str==6){
+              /*if(str==6){
                p.fabriquer();
-               commandeExecutee=true,
+               commandeExecutee=true;
                }
               if(str==7){
                p.afficherCarte();
@@ -94,16 +98,10 @@ public class Action{
               if(str==10){
                position.choixAction();
                commandeExecutee=true,
-               }
-               if(not commandeExecutee){
-                  System.out.println("Commande introuvable.")
+               }*/
+               if(!commandeExecutee){
+                  System.out.println("Commande introuvable.");
                   }
                Action.agir(p);
-         }
+         }}
 }
-              
-               
- 
-          
-         
-      
