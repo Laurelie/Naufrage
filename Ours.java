297204ac94a1c710bd19f92e928094281189d59b
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-25 16:18:08 +0200

    Update Ours.java

--- Ours.java
+++ Ours.java
@@ -1,9 +1,9 @@
-public class Ours extends Animaux implements /*Mangeable,*/ Chassable{  /*Animaux extend deja Mangeable, ca suffit non?*/
+public class Ours extends AnimauxChassable /*Mangeable,*/{  /*Animaux extend deja Mangeable, ca suffit non?*/
   public Ours(){
-    super("Ours", 100, int(Math.random()*10+10));
+    super("Ours", 100, 80, (int)(Math.random()*10)+10);
   }
   public void reagirAttaque( Personnage p){
-    if(Math.random()<0.8){
+    if(Math.random()<0.2){
       System.out.println("La griffe de l'ours vous manque de peu");
     }
     else{
@@ -11,7 +11,7 @@ public class Ours extends Animaux implements /*Mangeable,*/ Chassable{  /*Animau
       p.modifierSante(-20);
     }
   }
-  public void reagirFuite(Personnage p);
+  public void reagirFuite(Personnage p) {};
   public void descriptionMort(){
      System.out.println("L'ours s'effondre par terre, raide mort.");
   }
