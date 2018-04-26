d6f9cf747954a5c4eac51b951a91fbfbd2312ca0
Shibateur <38659350+Shibateur@users.noreply.github.com>
2018-04-26 11:04:13 +0200

    Update Personnage.java

--- Personnage.java
+++ Personnage.java
@@ -10,7 +10,11 @@ public class Personnage{
 	Je pense que c'est plus simple pour consulter tes armes avant un combat ou pour choisir ce que tu veux manger, de faire plusieurs listes, comme ca on affiche juste la liste*/
 	private ArrayList<Arme> equipement = new ArrayList<Arme>(2);
 	private int nbEquipements;
-	private ArrayList<Animaux> aliments = new ArrayList<Animaux>();
+	private final ArrayList<? implements Fabricable> listeConstruction = new ArrayList<? implements Fabricable>();
+	private ArrayList<? implements Mangeable> aliments = new ArrayList<? implements Mangeable>();
+	private ArrayList<Integer> nbAliments = new ArrayList<Integer>();
+	private ArrayList<Ressource> ressourcesNonComestibles = new ArrayList<Ressource>();
+	private ArrayList<Integer> nbRessources = new ArrayList<Integer>();
 	private int poidsInventaire; 
 	private /*final*/ int poidsMaxInventaire;/*augmentable en craftant sac a dos?*/
 	private int energie;
@@ -71,12 +75,12 @@ public class Personnage{
 	public void afficherInventaire(ArrayList<?> liste){ //LAU
 		System.out.println("Voici votre inventaire :\n");
 		for(int i=0; i<liste.size();i++){
-			System.out.println(i+1+") "+liste.get(i));
+			System.out.println((i+1)+") "+liste.get(i));//Attention ici tu vas juste afficher l'adresse de l'objet si c'est pas un type de base
 		}
 		System.out.println("");
 	}
     
- /* public void ramasser(? extends Stockable objet){
+  public void ramasser(? extends Stockable objet){
     	if (poidsInventaire+objet.getPoids() >= poidsMaxInventaire)
       		System.out.println("Vous ne pouvez pas stocker cet objet ("+objet.getPoids()+") : "+poidsInventaire+"/"+poidsMaxInventaire)
     	else{
@@ -86,7 +90,7 @@ public class Personnage{
   	}
   	public ArrayList<? extends Stockage> getInventaire(){
     	return inventaire;      
-  	}*/
+  	}
     
   	public void seDeplacer(String direction){
     	if(direction.equals("Nord")){
@@ -104,9 +108,36 @@ public class Personnage{
 	    carte[x][y].decrireLieu();
 	    carte[x][y].genererAnimal();
   	}
-    
- 	public void fabriquer(){ //afficher les objets que l'on peut fabriquer et les matériaux à avoir pour le faire ?
-
+    	public boolean estFabricable(objet implements Fabricable){
+		boolean estFabricable = false;
+		for(int i=0;i<(objet.listeMateriaux()).lenght();i++){
+			for(int j = 0; j<aliments.size(); j++){
+				if( (aliments.get(j)).toString()==((objet.listeMateriaux()).get(i)).toString() && nbAliments[j]>=(objet.quantiteMateriaux()).get(i)){
+					estFabricable = true;
+				}
+			for(int k = 0; k<ressourcesNonComestibles.size(); k++){
+				if( (ressourcesNonComestibles.get(k)).toString()==((objet.listeMateriaux()).get(i)).toString() && nbRessources[k]>=(objet.quantiteMateriaux()).get(i)){
+					estFabricable = true;
+				}
+			if(! estFabricable){
+				return false;
+			}
+			estFabricable = false;
+		}
+		return(true);
+	}
+				
+			
+			
+ 	public void fabriquer(){ //afficher les objets que l'on peut fabriquer et les matériaux à avoir pour le faire ? YEP
+		for(int i = 0; i<listeConstruction.size(); i++){
+			System.out.println(i+") "+(listeConstruction.get(i)).toString());
+		}
+		int str = Integer.parseInt(sc.nextLine());
+		
+			
+					
+					
   	}
   
   	public void manger(){ //LAU
