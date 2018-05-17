8f68890567b2899e5a19ab5d817bd92d1f54e078
Laurelie <laurelie.michielon@gmail.com>
2018-05-17 22:09:26 +0200

    Delete Chassable.java

--- Chassable.java
+++
@@ -1,6 +0,0 @@
-public interface Chassable{
-  public abstract void reagirAttaque( Personnage p);
-  public abstract void reagirFuite(Personnage p);
-  public abstract void descriptionMort();
-  /*public void reagirapprivoiser(Personnage p, ? implements Mangeable aliment);*/
-}
